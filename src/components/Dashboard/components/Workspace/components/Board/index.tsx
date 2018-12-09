bb4f80dd23b75f688df6b5da8ae44dd915c85bb3
Eduard Bardají Puig <ebardaji@gmail.com>
2018-12-09 17:03:29 +0100

    Fetch data from API

--- src/components/Dashboard/components/Workspace/components/Board/index.tsx
+++ src/components/Dashboard/components/Workspace/components/Board/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
+import { IPerson } from '../../../../../../types/randomUser';
 import { Card } from './components/Card';
 import * as Table from './components/Table';
 
+type THiringStage = 'applied' | 'interviewing' | 'hired';
+
+interface ICandidate extends IPerson {
+  hiringStage: THiringStage;
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD_DATA_FROM_API': {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
 export const Board = () => {
+  const [state, dispatch]: [ICandidate[], any] = useReducer(reducer, []);
+  useEffect(() => {
+    const operations = async () => {
+      const body = await fetch('https://randomuser.me/api/?nat=gb&results=5');
+      const data = (await body.json()).results as IPerson[];
+      const candidates: ICandidate[] = data.map(candidate => {
+        const hiringStage: THiringStage = 'applied';
+        return {
+          ...candidate,
+          hiringStage,
+        };
+      });
+      dispatch({ type: 'LOAD_DATA_FROM_API', payload: candidates });
+    };
+
+    operations();
+  }, []);
   return (
     <Table.Container>
       <Table.HeaderApplied>Applied</Table.HeaderApplied>
@@ -11,12 +46,20 @@ export const Board = () => {
       <Table.HeaderHired>Hired</Table.HeaderHired>
 
       <Table.CandidatesApplied>
-        <Card
-          name={{ first: 'John', last: 'Doe' }}
-          image="https://randomuser.me/api/portraits/women/51.jpg"
-          phone="+4415264898"
-          email="john.doe@gmail.com"
-        />
+        {state
+          .filter(candidate => {
+            const applied: THiringStage = 'applied';
+            return candidate.hiringStage === applied;
+          })
+          .map(candidate => (
+            <Card
+              key={candidate.id.value}
+              name={candidate.name}
+              image={candidate.picture.medium}
+              phone={candidate.phone}
+              email={candidate.email}
+            />
+          ))}
       </Table.CandidatesApplied>
 
       <Table.CandidatesInterviewing>
